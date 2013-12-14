9119c395561d0c3077e27d276a1252fe04bee040
Greg Look <greg@greg-look.net>
2013-12-14 15:11:48 -0800

    Quick formatting pass through the lib files.

--- lib/toolkit/config.rb
+++ lib/toolkit/config.rb
@@ -4,43 +4,41 @@ require 'pathname'
 require 'yaml'
 
 class Toolkit
-
-# A class to manage user-specific toolkit configuration and other persistent
-# state.
-#
-# Author:: Greg Look
-class Config
-  attr_reader :path, :selected
-  attr_accessor :installed, :links
-
-  # Loads toolkit configuration from the given file path.
+  # A class to manage user-specific toolkit configuration and other persistent
+  # state.
   #
-  # +path+:: location of config file to load
-  def initialize(path)
-    @path = Pathname.new(path)
+  # Author:: Greg Look
+  class Config
+    attr_reader :path, :selected
+    attr_accessor :installed, :links
 
-    config = @path.exist? && @path.open{|f| YAML.load(f) } || { }
+    # Loads toolkit configuration from the given file path.
+    #
+    # +path+:: location of config file to load
+    def initialize(path)
+      @path = Pathname.new(path)
 
-    @installed = config['installed'] || []
-    @selected  = config['selected']  || {}
-    @links     = config['links']     || {}
-  end
+      config = @path.exist? && @path.open{|f| YAML.load(f) } || { }
 
-  # Saves bag configuration.
-  def save
-    config = {
-      'installed' => @installed,
-      'selected'  => @selected,
-      'links'     => @links
-    }
+      @installed = config['installed'] || []
+      @selected  = config['selected']  || {}
+      @links     = config['links']     || {}
+    end
 
-    @path.parent.mkpath unless @path.parent.directory?
-    @path.open('w') do |file|
-      file.puts "# Toolkit configuration written #{Time.now}"
-      file.puts "# vim" + ": ft=yaml"
-      file.puts config.to_yaml
+    # Saves the toolkit configuration.
+    def save
+      config = {
+        'installed' => @installed,
+        'selected'  => @selected,
+        'links'     => @links
+      }
+
+      @path.parent.mkpath unless @path.parent.directory?
+      @path.open('w') do |file|
+        file.puts "# Toolkit configuration written #{Time.now}"
+        file.puts "# vim" + ": ft=yaml"
+        file.puts config.to_yaml
+      end
     end
   end
-end
-
 end
--- lib/toolkit/manifest.rb
+++ lib/toolkit/manifest.rb
@@ -4,43 +4,44 @@ require 'pathname'
 require 'toolkit/package'
 
 class Toolkit
-
-# A manifest is a collection of packages.
-#
-# Author:: Greg Look
-class Manifest
-  attr_reader :root, :packages
-
-  FILE = 'manifest.rb'.freeze
-
-  # Loads the manifest file in the given package root directory.
+  # A package set is a collection of packages, defined by a manifest file.
   #
-  # +root+:: location containing manifest file and packages
-  # +file+:: optional filename override
-  def initialize(root, file=FILE)
-    @root = Pathname.new(root).freeze
+  # Author:: Greg Look
+  class Manifest
+    attr_reader :root, :name, :packages
 
-    manifest = @root + file
-    raise "No readable manifest file at '#{manifest}'" unless manifest.readable?
+    MANIFEST_FILE = 'manifest.rb'.freeze
 
-    @packages = { }
-    instance_eval manifest.readlines.join, manifest.to_s, 1
-    @packages.freeze
+    # Loads the manifest file in the given package set directory.
+    #
+    # `root`:: location containing manifest file and packages
+    # `file`:: optional manifest filename override
+    def initialize(root, manifest=MANIFEST_FILE)
+      @root = Pathname.new(root).freeze
+      raise "No package set located at #{@root}" unless @root.dir?
+
+      @name = @root.basename.freeze
+
+      manifest_path = @root + manifest
+      raise "No readable package manifest at #{manifest_path}" unless manifest_path.readable?
+
+      @packages = { }
+      instance_eval manifest_path.readlines.join, manifest_path.to_s, 1
+      @packages.freeze
+    end
+
+    private
+
+    # Registers a package. Meant to be called from the manifest file.
+    def package(name, options={})
+      root = options[:root] || (@root + name)
+      @packages[name] = Package.new(name, root, options)
+    end
+
+    # Tests whether a command with the given name exists in the PATH.
+    def installed?(command)
+      `which #{command} 2> /dev/null`
+      $?.success?
+    end
   end
-
-  private
-
-  # Registers a package. Meant to be called from the manifest file.
-  def package(name, options={})
-    root = options[:root] || (@root + name)
-    @packages[name] = Package.new(name, root, options)
-  end
-
-  # Tests whether a command with the given name exists in the PATH.
-  def installed?(command)
-    `which #{command} 2> /dev/null`
-    $?.success?
-  end
-end
-
 end
